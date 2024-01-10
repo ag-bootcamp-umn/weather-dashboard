5be187681ce55436fe0657eafc316d8ee9c802c5
alejandro <alexgeorgegeer@gmail.com>
2024-01-09 23:59:53 -0600

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// ASSIGNMENT
+
+/*
+UI:
+<header>
+<input form>
+<block.past-searches></block.past-searches>
+<block.current-weather></block.current-weather>
+<block.5-day-forecast></block.5-day-forecast>
+
+FUNCTIONALITY
+
+search:
+    API call for specific city
+
+display:
+    Parse the data and use bits of it to display this and that
+
+store:
+    store previously-searched cities in local database
+
+renderButtons:
+    Get stored data of past searches
+    render buttons with:
+        name
+        url
+
+
+
+*/
